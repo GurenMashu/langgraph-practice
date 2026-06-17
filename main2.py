a915736e5b3013b31a8f35421e6f2e4c9059d9a7
Glenn Mathews <glenn.mathews@acsiatech.com>
2026-06-17 15:27:38 +0530

    minor updates

--- main2.py
+++ main2.py
@@ -6,16 +6,22 @@ from typing import TypedDict, Annotated, Literal
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
 from langgraph.checkpoint.memory import InMemorySaver
-from langchain.chat_model import init_chat_model
+from langchain.chat_models import init_chat_model
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_core.documents import Document
+from langchain_openai import OpenAIEmbeddings
 
-laod_dotenv()
+load_dotenv()
 
 llm = init_chat_model("openai:gpt-4.1-mini")
 
 K_Base = []
 
+vector_store = InMemoryVectorStore(OpenAIEmbeddings(model = "text-embedding-3-small"))
+vector_store.add_documents([Document(page_content = text) for text in K_Base])
+
 class IntentClassifier(BaseModel):
-	message_intent: Literal["chat", "knowledge", "code"] = Field(..., description = ["Classify whether the user wants to chat, ask for knowledge or change code in the project"]
+	message_intent: Literal["chat", "knowledge", "code"] = Field(..., description = ["Classify whether the user wants to chat, ask for knowledge or change code in the project"])
 
 class State(TypedDict):
 	messages: Annotated[list, add_messages]
@@ -24,7 +30,7 @@ class State(TypedDict):
 def classify_intent(state: State):
 	structured_llm = llm.with_structured_output(IntentClassifier)
 	result = structured_llm.invoke([
-		{"role": "system", "content": "Determine/Classify whether the user wants to chat ("chat"), retrieve knowledge ("knowledge") or change code ("code")},
+		{"role": "system", "content": "Determine/Classify whether the user wants to chat ('chat'), retrieve knowledge ('knowledge') or change code ('code')"},
 		{"role": "user", "content": state["messages"][-1].content}
 	])
 
@@ -36,7 +42,11 @@ def prompt_llm_chat(state: State):
 	return {"messages": [{"role": "assistant", "content": response.content}]}
 
 def prompt_llm_rag(state: State):
-	messages = [{"role": "system", "content": "No matter what, always say 'I am the Rag Bot'"}] + state["messages"]
+	query = state["messages"][-1].content
+	documents = vector_store.similarity_search(query, k = 3)
+	context = "".join(f" -{doc.page_content}" for doc in documents)
+
+	messages = [{"role": "system", "content": f"You are a RAG agent. Answer the user using only the context below. If the answer is not in the context then say you dont know.\n\nContext:\n {context}"}] + state["messages"]
 	response = llm.invoke(messages)
 	return {"messages": [{"role": "assistant", "content": response.content}]}
 
@@ -52,7 +62,7 @@ graph_builder.add_node("chat_agent", prompt_llm_chat)
 graph_builder.add_node("rag_agent", prompt_llm_rag)
 graph_builder.add_node("coding_agent", prompt_llm_code)
 
-graph_builder.add_edge(START, "classifier)
+graph_builder.add_edge(START, "classifier")
 graph_builder.add_conditional_edge("classifier", lambda state: state["message_intent"], {"chat": "chat_agent", "knowledge": "rag_agent", "code": "coding_agent"})
 graph_builder.add_edge("chat_agent", END)
 graph_builder.add_edge("rag_agent", END)
@@ -60,6 +70,8 @@ graph_builder.add_edge("code_agent", END)
 
 checkpointer = InMemorySaver()
 graph = graph_builder.compile(checkpointer = checkpointer)
+
+graph.get_graph().draw_mermaid_png(output_file_path = "graph.png")
 
 config = {"configurable": {"thread_id": uuid.uuid4()}}
 
